964bb14601746eaabfaa3a0c75ba52d70782e9ad
Gyozmo <binantguillaume@gmail.com>
2018-04-30 22:21:45 +0200

    First commit

---
+++ Todolist/script.js
@@ -0,0 +1,34 @@
+
+
+/*
+var add = document.getElementById('ajouter')
+
+
+
+
+function todo() {
+
+  var formContent = document.getElementById('text1').value
+
+
+
+    var newParagraph = document.createElement('p')
+
+    newParagraph.textContent = formContent
+
+    document.getElementById("element").appendChild('p')
+    document.getElementById('element').innerHTML = formContent;
+
+}
+
+
+
+console.log(formContent);
+
+
+*/
+
+var inputValue = 
+
+var p = document.createElement("p")
+document.getElementById('element').appendChild(p)
